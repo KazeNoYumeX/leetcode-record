9c357dd692aa6ce19156949c7703b286c24af857
KazeNoYumeX <foever527@gmail.com>
2022-08-04 02:06:10 +0800

    feat: Add solutions for No.104 Maximum Depth of Binary Tree in JavaScript & PHP

---
+++ PHP/104. Maximum Depth of Binary Tree.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxDepth($root)
+    {
+        return empty($root) ? 0 : max($this->maxDepth($root->left), $this->maxDepth($root->right)) + 1;
+    }
+}
--- SUMMARY.md
+++ SUMMARY.md
@@ -13,6 +13,7 @@
 * [20. Valid Parentheses](problems/20.-valid-parentheses.md)
 * [26. Remove Duplicates from Sorted Array](problems/26.-remove-duplicates-from-sorted-array.md)
 * [46. Permutations](problems/46.-permutations.md)
+* [104. Maximum Depth of Binary Tree](problems/104.-maximum-depth-of-binary-tree.md)
 * [509. Fibonacci Number](problems/509.-fibonacci-number.md)
 * [1710. Maximum Units on a Truck](problems/1710.-maximum-units-on-a-truck.md)
 * [1929. Concatenation of Array](problems/1929.-concatenation-of-array.md)
