6adc5fa66e109d0ec287220ae18b41638aaac16d
KazeNoYumeX <foever527@gmail.com>
2022-09-10 10:34:24 +0800

    feat: Add solution for No.1 Two Sum in Python

---
+++ Python/1. Two Sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tmp = {}
+
+        for i, num in enumerate(nums):
+            if target - num in tmp:
+                return [tmp[target - num], i]
+            tmp[num] = i
+
+        return [-1, -1]
