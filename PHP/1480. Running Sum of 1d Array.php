7ade95c82a6e80c5f983e3118c9e89b7ceebc9d0
KazeNoYumeX <foever527@gmail.com>
2022-10-16 16:28:09 +0800

    feat: Add solutions for No.1480 Running Sum of 1d Array in TypeScript, PHP and Python

---
+++ PHP/1480. Running Sum of 1d Array.php
@@ -0,0 +1,29 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param $carry
+     * @param $item
+     * @return array
+     */
+    function sum($carry, $item): array
+    {
+        if (empty($carry)) {
+            return array($item);
+        }
+
+        $target = count($carry) - 1;
+        $carry[] = $carry[$target] + $item;
+        return $carry;
+    }
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function runningSum(array $nums): array
+    {
+        return array_reduce($nums, array($this, 'sum'));
+    }
+}
---
+++ TypeScript/1480. Running Sum of 1d Array.ts
@@ -0,0 +1,4 @@
+const runningSum = (nums: number[]): number[] => {
+    nums.reduce((accumulator: number, current: number, idx: number, arr: number[]) => arr[idx] += accumulator)
+    return nums
+};
