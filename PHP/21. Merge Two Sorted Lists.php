bb1bb35957f4233065ceba20572170babd36a455
KazeNoYumeX <foever527@gmail.com>
2022-10-21 06:30:21 +0800

    feat: Add solutions for No.21 Merge Two Sorted Lists in TypeScript, PHP and Python

---
+++ PHP/21. Merge Two Sorted Lists.php
@@ -0,0 +1,38 @@
+<?php
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
+    /**
+     * @param ListNode|null $list1
+     * @param ListNode|null $list2
+     * @return ListNode
+     */
+    function mergeTwoLists(?ListNode $list1, ?ListNode $list2): ?ListNode
+    {
+        if (empty($list1))
+            return $list2;
+
+        if (empty($list2))
+            return $list1;
+
+        if ($list1->val < $list2->val) {
+            $list1->next = $this->mergeTwoLists($list1->next, $list2);
+            return $list1;
+        } else {
+            $list2->next = $this->mergeTwoLists($list1, $list2->next);
+            return $list2;
+        }
+    }
+}
