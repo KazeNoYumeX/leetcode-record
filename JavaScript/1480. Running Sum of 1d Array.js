3bda6278a15dc350594a0e0481cdca5bb9c082b2
KazeNoYumeX <foever527@gmail.com>
2022-11-02 13:51:44 +0800

    feat: Add solution for No.1480 Running Sum of 1d Array in JavaScript

---
+++ JavaScript/1480. Running Sum of 1d Array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = (nums) => {
+    nums.reduce((accumulator, current, idx, arr) => arr[idx] += accumulator)
+    return nums
+};
