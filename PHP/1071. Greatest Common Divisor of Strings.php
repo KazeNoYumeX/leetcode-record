7361ab852309156d1c85b272eaaa0bd90069bc3b
KazeNoYumeX <foever527@gmail.com>
2023-10-04 16:58:13 +0800

    feat: Add solutions for No.1071 Greatest Common Divisor of Strings in TypeScript, PHP and Python

---
+++ PHP/1071. Greatest Common Divisor of Strings.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+class Solution
+{
+    public function gcd(int $a, int $b): int
+    {
+        // Euclidean Algorithm
+        return $b === 0 ? $a : $this->gcd($b, $a % $b);
+    }
+
+    /** @noinspection PhpUnused */
+    public function gcdOfStrings(string $str1, string $str2): string
+    {
+        if ($str1 . $str2 !== $str2 . $str1) {
+            return '';
+        }
+
+        return substr($str1, 0, $this->gcd(strlen($str1), strlen($str2)));
+    }
+}
