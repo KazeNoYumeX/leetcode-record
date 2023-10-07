c498fd004051b53c4dda36988499724c79947ef0
KazeNoYumeX <foever527@gmail.com>
2023-10-07 16:01:33 +0800

    feat: Add solution for No.1431 Kids With the Greatest Number of Candies in PHP

--- PHP/1431. Kids With the Greatest Number of Candies.php
+++ PHP/1431. Kids With the Greatest Number of Candies.php
@@ -1,18 +1,18 @@
 <?php
 
 /** @noinspection PhpMultipleClassDeclarationsInspection */
-
 class Solution
 {
     /**
-     * @param Integer[] $candies
-     * @param Integer $extraCandies
-     * @return Boolean[]
+     * @param  int[]  $candies
+     * @return bool[]
+     *
      * @noinspection PhpUnused
      */
-    function kidsWithCandies(array $candies, int $extraCandies): array
+    public function kidsWithCandies(array $candies, int $extraCandies): array
     {
         $max = max($candies);
-        return array_map(fn($candy) => $candy + $extraCandies >= $max, $candies);
+
+        return array_map(fn ($candy) => $candy + $extraCandies >= $max, $candies);
     }
 }
