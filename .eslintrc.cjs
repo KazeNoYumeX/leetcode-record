fb551af6787c1dc21cf862ad320382844cf840d0
KazeNoYumeX <foever527@gmail.com>
2023-11-02 18:18:18 +0800

    chore: Modify tsconfig.json settings, and update dependent packages regularly

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
+    rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+    }
+};
