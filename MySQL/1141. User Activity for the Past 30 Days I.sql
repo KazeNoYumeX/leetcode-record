97253a928b09565ca50ad7425903459dd11f8f42
KazeNoYumeX <foever527@gmail.com>
2023-11-19 03:29:38 +0800

    feat: Add solution for No.1141 User Activity for the Past 30 Days I in MySQL

--- MySQL/1141. User Activity for the Past 30 Days I.sql
+++ MySQL/1141. User Activity for the Past 30 Days I.sql
@@ -44,7 +44,7 @@ values ('4', '3', '2019-06-25', 'end_session');
 
 # Solution
 select activity_date as day, count(distinct user_id) as active_users
-from activity
+from Activity
 where datediff('2019-07-27', activity_date) < 30
   and activity_date <= '2019-07-27'
-group by activity_date
+group by activity_date;
