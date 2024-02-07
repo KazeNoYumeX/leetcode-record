028f58436848c6454dd3f49c4071407da55bd4c1
KazeNoYumeX <foever527@gmail.com>
2024-02-07 20:44:25 +0800

    feat: Add solution for No.197 Rising Temperature in MySQL

--- MySQL/197. Rising Temperature.sql
+++ MySQL/197. Rising Temperature.sql
@@ -23,4 +23,4 @@ values ('4', '2015-01-04', '30');
 # Solution
 select w1.id
 from Weather as w1
-         join Weather as w2 on datediff(w1.recordDate, w2.recordDate) = 1 AND w1.Temperature > w2.Temperature;
+         join Weather as w2 on datediff(w1.recordDate, w2.recordDate) = 1 and w1.Temperature > w2.Temperature;
