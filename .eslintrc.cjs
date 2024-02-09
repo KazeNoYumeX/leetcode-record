b2877163b46914b7bdc35a3bba35bfee53528cbb
KazeNoYumeX <foever527@gmail.com>
2024-02-09 17:50:58 +0800

    style: Use prettier to optimize the project code writing style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     },
     root: true,
     rules: {
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-    }
-};
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+    },
+}
