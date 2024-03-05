d56e72bb5dd090081c40afc4757648edcf5cb9cf
KazeNoYumeX <foever527@gmail.com>
2024-03-05 17:18:04 +0800

    feat: Add solutions for No.27 Remove Element in PHP and Go

---
+++ PHP/27. Remove Element.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *  @noinspection PhpIllegalPsrClassPathInspection, PhpMultipleClassDeclarationsInspection
+ */
+class Solution
+{
+    /**
+     * @param  int[]  $nums
+     *
+     * @noinspection PhpUnused
+     */
+    public function removeElement(array &$nums, int $val): int
+    {
+        $i = 0;
+        for ($j = 0; $j < count($nums); $j++) {
+            if ($nums[$j] !== $val) {
+                $nums[$i++] = $nums[$j];
+            }
+        }
+
+        return $i;
+
+    }
+}
