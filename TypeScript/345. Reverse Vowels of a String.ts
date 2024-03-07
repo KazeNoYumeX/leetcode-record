8682914c955955f7eb1c0883a2dd7dcc3aeb3d1f
KazeNoYumeX <foever527@gmail.com>
2024-03-07 15:31:12 +0800

    feat: Add solutions for No.345 Reverse Vowels of a String in JavaScript and TypeScript

--- TypeScript/345. Reverse Vowels of a String.ts
+++ TypeScript/345. Reverse Vowels of a String.ts
@@ -1,36 +1,36 @@
 // noinspection JSUnusedGlobalSymbols
 const reverseVowels = (s: string): string => {
-    const length = s.length
+  const length = s.length
 
-    if (length <= 1) {
-        return s
+  if (length <= 1) {
+    return s
+  }
+
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+  let left = 0
+  let right = length - 1
+  const arr = s.split('')
+
+  while (left < length) {
+    if (left >= right) {
+      break
     }
 
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
-    let left = 0
-    let right = length - 1
-    const arr = s.split('')
-
-    while (left <length){
-        if (left >= right) {
-            break
+    if (vowels.has(s[left].toLowerCase())) {
+      while (right > left) {
+        if (vowels.has(s[right].toLowerCase())) {
+          arr[left] = s[right]
+          arr[right] = s[left]
+          right--
+          break
         }
 
-        if (vowels.has(s[left].toLowerCase())) {
-            while (right > left) {
-                if (vowels.has(s[right].toLowerCase())) {
-                    arr[left] = s[right]
-                    arr[right] = s[left]
-                    right--
-                    break
-                }
-
-                right--
-            }
-        }
-
-        left++
+        right--
+      }
     }
 
-    return arr.join('')
+    left++
+  }
+
+  return arr.join('')
 }
