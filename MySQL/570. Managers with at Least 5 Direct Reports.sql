4dca8748428b4e0ccc1267318c2f47250e3425f7
KazeNoYumeX <foever527@gmail.com>
2024-03-09 22:45:29 +0800

    feat: Add solution for No.570 Managers with at Least 5 Direct Reports in MySQL

--- MySQL/570. Managers with at Least 5 Direct Reports.sql
+++ MySQL/570. Managers with at Least 5 Direct Reports.sql
@@ -31,5 +31,5 @@ values ('106', 'Ron', 'B', '101');
 select e1.name
 from Employee as e1
          inner join Employee as e2 on e1.id = e2.managerId
-group by e1.name, e2.managerId
-having count(e1.name) >= 5;
+group by e2.managerId
+having count(e2.managerId) >= 5;
