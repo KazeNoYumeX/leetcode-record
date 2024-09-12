a267e06341bda11a3311d121671a695a7b5214f7
KazeNoYumeX <foever527@gmail.com>
2024-09-12 13:37:56 +0800

    feat: Add solution for No.1661 Average Time of Process per Machine in MySQL

--- MySQL/1661. Average Time of Process per Machine.sql
+++ MySQL/1661. Average Time of Process per Machine.sql
@@ -49,6 +49,8 @@ values ('2', '1', 'end', '5');
 select a1.machine_id, round(avg(a1.timestamp - a2.timestamp), 3) as processing_time
 from Activity as a1
          join Activity as a2
-              on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type = 'end' and
-                 a2.activity_type = 'start'
+              on a1.machine_id = a2.machine_id
+                  and a1.process_id = a2.process_id
+                  and a1.activity_type = 'end'
+                  and a2.activity_type = 'start'
 group by a1.machine_id;
