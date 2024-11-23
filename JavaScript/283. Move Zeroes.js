d7fa946a6d359379b81e4c19f3cffd28a4f09671
KazeNoYumeX <foever527@gmail.com>
2024-11-23 11:30:32 +0800

    feat: Add solutions for No.283 Move Zeroes in PHP, TypeScript and JavaScript

--- JavaScript/283. Move Zeroes.js
+++ JavaScript/283. Move Zeroes.js
@@ -1,10 +1,14 @@
 // noinspection JSUnusedGlobalSymbols
-const moveZeroes = (nums: number[]): void => {
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function (nums) {
   const length = nums.length
   let zeroSize = 0
 
   for (let i = 0; i < length; i++) {
-    if (nums[i] == 0) {
+    if (nums[i] === 0) {
       zeroSize++
     } else if (zeroSize > 0) {
       nums[i - zeroSize] = nums[i]
