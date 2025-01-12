d4a0e6d424586cf83642844cc93b7d59fed26fcf
KazeNoYumeX <foever527@gmail.com>
2025-01-12 16:15:27 +0800

    feat: Add solution for No.238 Product of Array Except Self in PHP

--- PHP/238. Product of Array Except Self.php
+++ PHP/238. Product of Array Except Self.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- *  @noinspection PhpIllegalPsrClassPathInspection, PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpUnused
  */
 class Solution
 {
     /**
      * @param  int[]  $nums
      * @return int[]
-     *
-     * @noinspection PhpUnused
      */
     public function productExceptSelf(array $nums): array
     {
