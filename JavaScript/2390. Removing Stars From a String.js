08c8cbd047c23e08834d160d6cf55f8567f7d97b
KazeNoYumeX <foever527@gmail.com>
2025-02-02 18:04:13 +0800

    feat: Add solutions for No.2390 Removing Stars From a String in PHP, TypeScript and JavaScript

--- JavaScript/2390. Removing Stars From a String.js
+++ JavaScript/2390. Removing Stars From a String.js
@@ -1,6 +1,11 @@
 // noinspection JSUnusedGlobalSymbols
-const removeStars = (s: string): string => {
-  const arr: string[] = []
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const removeStars = (s) => {
+  const arr = []
 
   for (let i = 0; i < s.length; i++) {
     const word = s[i]
