750247efcc471ea280e493294803fc53189d0d37
KazeNoYumeX <foever527@gmail.com>
2025-02-09 18:58:43 +0800

    feat: Add solutions for No.278 First Bad Version in PHP, TypeScript and Python

--- TypeScript/278. First Bad Version.ts
+++ TypeScript/278. First Bad Version.ts
@@ -1,20 +1,16 @@
-/**
- * The knows API is defined in the parent class Relation.
- * isBadVersion(version: number): boolean {
- *     ...
- * };
- */
+// noinspection JSUnusedGlobalSymbols
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const solution = (isBadVersion: any) => {
-    return (n: number): number => {
-        let curr: number = 1
-        while (curr < n) {
-            const mid = Math.floor((curr + n) / 2)
-            if (isBadVersion(mid)) {
-                n = mid
-            } else {
-                curr = mid + 1
-            }
-        }
-        return curr
+  return (n: number): number => {
+    let left = 1
+    while (left <= n) {
+      const mid = left + Math.floor((n - left) / 2)
+      if (isBadVersion(mid)) {
+        n = mid - 1
+      } else {
+        left = mid + 1
+      }
     }
+    return left
+  }
 }
